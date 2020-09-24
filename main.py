54750ab8792abf9af1ed75eaf81b3286b937823d
HEQIN11 <hbw5248@psu.edu>
2020-09-24 22:06:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+# Author: Heqin Wang hbw5248@psu.edu
+# Collaborator:Arabella Harrison axh5810@psu.edu
+# Collaborator:Ryan Dang rvd5465@psu.edu
+# Collaborator:Abel Ismael agi5031@psu.edu
+# Section: 12
+# Breakout: 6
+
+def num_of_divisors(n):
+ 
+  s=n
+  a=0
+
+  while n>0:
+    if s%n==0:
+      n=n-1
+      a=a+1
+    else:
+      n=n-1
+  return a
+
+def num_of_primes(n):
+
+  for i in range(n,0,-1):
+    if num_of_divisors(i)==2:
+      return i
+      
+  
+
+def sum_n(n):
+
+  s=0
+  while n>0:
+    s=s+n
+    n=n-1 
+  return s
+
+def print_n(s, n):
+ 
+  for n in range(n,0,-1):
+    if n>0:
+      print(f"{s}")
+  return
+
+def run():
+  num = int(input("Enter an int: "))
+  print(f"sum is {sum_n(num)}.")
+  print(f"{num} has {num_of_divisors(num)} divisors.")
+  print(f"There are {num_of_primes(num)} primes <= {num}.")
+  line = input("Enter a string: ")
+  print_n(line, num)
+
+if __name__ == "__main__":
+  run()
